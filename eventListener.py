ce1e9b3a930c50bae0f0fff56a7076bb72cf7f27
ITRose225 <itrose225@gmail.com>
2024-08-16 10:30:49 -0500

    i saw how to make it better in a dream

--- eventListener.py
+++ eventListener.py
@@ -1,27 +1,11 @@
-import twitchio
-from twitchio.ext import commands
+from flask import Flask
+import main
 
-# Define your Twitch API credentials
-client_id = 'YOUR_CLIENT_ID'
-client_secret = 'YOUR_CLIENT_SECRET'
-access_token = 'YOUR_ACCESS_TOKEN'
-refresh_token = 'YOUR_REFRESH_TOKEN'
+app = Flask("ShockCollar")
 
-# Create a Twitch bot instance
-bot = commands.Bot(
-    irc_token=access_token,
-    client_id=client_id,
-    client_secret=client_secret,
-    nick='YOUR_BOT_USERNAME',
-    prefix='!',
-    initial_channels=['YOUR_CHANNEL_NAME']
-)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
-# Event handler for when a bit event is received
-@bot.event
-async def event_bits(ctx, bits):
-    # Handle the bit event here
-    print(f'Received {bits} bits!')
-
-# Start the bot
-bot.run()
+if __name__ == '__main__':
+    app.run()
--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from flask import Flask, request
 from markupsafe import escape
-# from twitch import t
-
-app = Flask(__name__)
+# from twitch import twitch
 
 shockLevel = 0
 
 
-@app.route("/shock",methods=['GET'])
 def getShockLevel():
     return str(shockLevel)
 
-@app.route("/shock", methods=['POST'])
 def shock(power):
     #insert output to pin to change power
     #then shock
