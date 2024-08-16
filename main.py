481265a0559f4360e77e2c329f1430f13c049108
ITRose225 <itrose225@gmail.com>
2024-08-16 03:06:11 -0500

    added some basic routes

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from markupsafe import escape
+# from twitch import t
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    print ("Hello, World!")
-    return "<p>Hello, World!</p>"
+shockLevel = 0
+
+
+@app.route("/shock",methods=['GET'])
+def getShockLevel():
+    return str(shockLevel)
+
+@app.route("/shock", methods=['POST'])
+def shock(power):
+    #insert output to pin to change power
+    #then shock
+    return
+
+def changePower(power):
+    
+    return
